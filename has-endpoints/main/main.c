11447c2eda5d9b8565a537196a7c821f35ec28c7
Rodrigo Costa <rodrigo@localhost.localdomain>
2019-11-22 22:27:43 -0300

    Added AWS IoT Support - Added occupancy_sensor + temperature_sensor independent tasks

--- has-endpoints/main/main.c
+++ has-endpoints/main/main.c
@@ -1,22 +1,88 @@
+/*
+ * Copyright 2010-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Additions Copyright 2016 Espressif Systems (Shanghai) PTE LTD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+/**
+ * @file subscribe_publish_sample.c
+ * @brief simple MQTT publish and subscribe on the same topic
+ *
+ * This example takes the parameters from the build configuration and establishes a connection to the AWS IoT MQTT Platform.
+ * It subscribes and publishes to the same topic - "test_topic/esp32"
+ *
+ * Some setup is required. See example README for details.
+ *
+ */
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <limits.h>
+#include <string.h>
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/event_groups.h"
 #include "freertos/queue.h"
+
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event_loop.h"
+#include "esp_log.h"
+#include "esp_vfs_fat.h"
+#include "driver/sdmmc_host.h"
 #include "esp_types.h"
 #include "driver/gpio.h"
 #include "driver/adc.h"
 #include "esp_adc_cal.h"
 
-#define GPIO_OUTPUT_IO_0    2 // Board LED
-#define GPIO_OUTPUT_IO_1    17 
-#define GPIO_OUTPUT_PIN_SEL  ((1ULL<<GPIO_OUTPUT_IO_0) | (1ULL<<GPIO_OUTPUT_IO_1))
-#define GPIO_INPUT_IO_0     16
-#define GPIO_INPUT_PIN_SEL  (1ULL<<GPIO_INPUT_IO_0)
-#define ESP_INTR_FLAG_DEFAULT 0
-#define N_MULTISAMPLES	32
-#define DEFAULT_VREF    1100        
+#include "nvs.h"
+#include "nvs_flash.h"
+
+#include "aws_iot_config.h"
+#include "aws_iot_log.h"
+#include "aws_iot_version.h"
+#include "aws_iot_mqtt_client_interface.h"
+
+#include "nvs.h"
+#include "nvs_flash.h"
+
+#include "aws_iot_config.h"
+#include "aws_iot_log.h"
+#include "aws_iot_version.h"
+#include "aws_iot_mqtt_client_interface.h"
+
+
+#define GPIO_OUTPUT_IO_0    	2 // Board LED
+#define GPIO_OUTPUT_IO_1    	17 
+#define GPIO_OUTPUT_PIN_SEL  	((1ULL<<GPIO_OUTPUT_IO_0) | (1ULL<<GPIO_OUTPUT_IO_1))
+
+#define GPIO_INPUT_IO_0     	16
+#define GPIO_INPUT_PIN_SEL 		(1ULL<<GPIO_INPUT_IO_0)
+
+#define ESP_INTR_FLAG_DEFAULT 	0
+#define N_MULTISAMPLES			32
+#define N_STRING				50
+#define DEFAULT_VREF    		1100        
+
+static char mqtt_string[50];
+///#define TEMPERATURE_SENSOR
+#define OCCUPANCY_SENSOR
+#define IOT
+
+#if defined (IOT)
+#include "aws_iot.h"
+#endif
 
 static esp_adc_cal_characteristics_t *adc_chars_temperature;
 static esp_adc_cal_characteristics_t *adc_chars_internal_temperature;
@@ -28,14 +94,19 @@ void occupancy_sensor_init(void);
 void temperature_sensor_init(void);
 void internal_temperature_sensor_init(void);
 
-/// Interrupt ///
+
+/*
+ * Interrupts
+ */
 static void IRAM_ATTR gpio_isr_handler(void* arg) {
     uint32_t gpio_num = (uint32_t) arg;
     xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
 }
 
-static void occupancy_sensor_task(void* arg)
-{
+/*
+ * FreeRTOS tasks
+ */
+static void occupancy_sensor_task(void* arg) {
     static uint32_t io_num;
     static uint32_t actual_io_value = 0;
     static uint32_t previous_io_value = 0;
@@ -46,30 +117,32 @@ static void occupancy_sensor_task(void* arg)
 			if(actual_io_value != previous_io_value) {
 				// Occupancy sensor change event
 				//printf("GPIO[%d] intr, val: %d\r\n", io_num, gpio_get_level(io_num));
-				if(actual_io_value) { // EVENT1 - Without presence
-					
+				if(actual_io_value) { // EVENT1 - Without presence	
 					printf("Porta ABERTA!\r\n");
+					sprintf(mqtt_string, "Porta ABERTA!\r\n");
 				}
 				else { // EVENT2 - Presence
 					printf("Porta FECHADA\r\n");
+					sprintf(mqtt_string, "Porta FECHADA!\r\n");
 				}
 			} // if(actual_io_value == previous_io_value)
     	}
-		vTaskDelay(1000 / portTICK_RATE_MS);
+		vTaskDelay(100 / portTICK_RATE_MS);
     }
 }
-static void temperature_sensor_task(void* arg)
-{
-    adc1_config_width(ADC_WIDTH_BIT_12);
-    adc1_config_channel_atten(ADC1_CHANNEL_7, ADC_ATTEN_DB_0);
-	adc_chars_temperature = calloc(1, sizeof(esp_adc_cal_characteristics_t));
-    val_type_temperature = esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_0, ADC_WIDTH_BIT_12, 1100, adc_chars_temperature);
-    
-    static uint16_t adc1_raw[N_MULTISAMPLES]; 
+
+static void temperature_sensor_task(void* arg) {
+	static uint16_t adc1_raw[N_MULTISAMPLES]; 
 	static uint16_t adc1_md = 0;
 	static uint32_t adc1_voltage_raw = 0;
 	static uint32_t adc1_voltage_md = 0;
 	float temperature = 0;
+
+    adc1_config_width(ADC_WIDTH_BIT_12);
+    adc1_config_channel_atten(ADC1_CHANNEL_7, ADC_ATTEN_DB_0);
+	adc_chars_temperature = calloc(1, sizeof(esp_adc_cal_characteristics_t));
+    val_type_temperature = esp_adc_cal_characterize(
+		ADC_UNIT_1, ADC_ATTEN_DB_0, ADC_WIDTH_BIT_12, DEFAULT_VREF, adc_chars_temperature);
 
 	for(;;) {
 		adc1_md = 0;
@@ -83,19 +156,20 @@ static void temperature_sensor_task(void* arg)
 		temperature = (float)(((float)adc1_voltage_md / 10.0) + (adc1_voltage_md / 100)) - 2;
 		
 		printf("ADC1_RAW=%umV\tADC1_MED=%umV\tT=%.1f°C\r\n", adc1_voltage_raw, adc1_voltage_md, temperature);
-		vTaskDelay(1000 / portTICK_RATE_MS);
+		sprintf(mqtt_string, "ADC1_RAW=%umV\tADC1_MED=%umV\tT=%.1f°C\r\n", adc1_voltage_raw, adc1_voltage_md, temperature);
+		vTaskDelay(5000 / portTICK_RATE_MS);
 	}
 }
 
 static void internal_temperature_sensor_task(void* arg) {
 	static uint16_t adc_hall_raw[N_MULTISAMPLES]; 
-    adc1_config_width(ADC_WIDTH_BIT_12);
     static uint16_t adc_hall = 0; 
 	static uint16_t adc_hall_md = 0; 
 	static uint32_t adc_hall_voltage = 0;
-
+	adc1_config_width(ADC_WIDTH_BIT_12);
 	adc_chars_internal_temperature = calloc(1, sizeof(esp_adc_cal_characteristics_t));
-	val_type_internal_temperature = esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_0, ADC_WIDTH_BIT_12, 1100, adc_chars_internal_temperature);
+	val_type_internal_temperature = esp_adc_cal_characterize(
+	ADC_UNIT_1, ADC_ATTEN_DB_0, ADC_WIDTH_BIT_12, DEFAULT_VREF, adc_chars_internal_temperature);
 	
 	for(;;) {
 		for(uint8_t i = 0; i < N_MULTISAMPLES; i++) {
@@ -105,24 +179,49 @@ static void internal_temperature_sensor_task(void* arg) {
     	adc_hall_md /= N_MULTISAMPLES;
 		adc_hall_voltage = esp_adc_cal_raw_to_voltage(adc_hall_md, adc_chars_internal_temperature);
 		printf("ADC_HALL=%umV\t%u\r\n", adc_hall, adc_hall_voltage);
+		sprintf(mqtt_string, "ADC_HALL=%umV\t%u\r\n", adc_hall, adc_hall_voltage);
 		vTaskDelay(1000 / portTICK_RATE_MS);
 	}
 }
  
 /// App main ///
 void app_main() {
-	internal_temperature_sensor_init();
+	    // Initialize NVS.
+#if defined (IOT)
+    esp_err_t err = nvs_flash_init();
+    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        err = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK( err );
+
+    initialise_wifi();
+    xTaskCreatePinnedToCore(&aws_iot_task, "aws_iot_task", 9216, NULL, 5, NULL, 1);
+#endif
+	//internal_temperature_sensor_init();
+#if defined (OCCUPANCY_SENSOR)
     occupancy_sensor_init();
-//#if defined (TEMPERATURE_SENSOR)
+#endif
+#if defined (TEMPERATURE_SENSOR)
 	temperature_sensor_init();
-//#endif
+#endif
+#if defined (RELAY_ACTUATOR)
+	relay_actuator_init();
+#endif
+	// Suspends itself
+	vTaskSuspend(NULL);
+	// Should never get here
+	printf("ERROR");
+	while(1);
 }
+
 /*
  *
  * 
  */
 void occupancy_sensor_init(void) {
 	gpio_config_t gpio_conf;
+	/*
 	//disable interrupt
 	gpio_conf.intr_type = GPIO_PIN_INTR_DISABLE;
 	//set as output mode
@@ -135,7 +234,7 @@ void occupancy_sensor_init(void) {
 	gpio_conf.pull_up_en = 0;
 	//configure GPIO with the given settings
 	gpio_config(&gpio_conf);
-
+	*/
 	//interrupt of rising edge
 	gpio_conf.intr_type = GPIO_PIN_INTR_POSEDGE;
 	//bit mask of the pins, use GPIO4/5 here
@@ -154,15 +253,13 @@ void occupancy_sensor_init(void) {
     gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
     //install gpio isr service
 
-
-	//start gpio task
-    xTaskCreate(occupancy_sensor_task, "OCUPPANCY_SENSOR_TASK", 2048, NULL, 1, NULL);
-	
-	
+		
 	gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
     //hook isr handler for specific gpio pin
     gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void*) GPIO_INPUT_IO_0);
-	
+
+	//start gpio task
+    xTaskCreate(occupancy_sensor_task, "OCUPPANCY_SENSOR_TASK", 2048, NULL, 1, NULL);
 }
 
 void temperature_sensor_init(void) {
